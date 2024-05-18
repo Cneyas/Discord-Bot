675160cfa74ad927cfb952c5dfb84de85f197ddc
Cneyas <56773160+Cneyas@users.noreply.github.com>
2024-05-18 13:47:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ prefix = '!'
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=prefix, intents=intents)
 
+initial_extensions = ['commands.commands']
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        client.load_extension(extension)
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} aktif')
-
 
 client.run(TOKEN)
